5c121d2b63daa196fff19cfad1133538bd1cfdac
Blake Gentry <blakesgentry@gmail.com>
2011-08-29 23:51:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DreamforceDemo::Application.config.secret_token = 'cf828e812f1ede8a620cb7c89a8c109268fd9fdb794644634b89571156cc32eed299a3799b9d1a8b563cda5a1e8eb11de20545c21b2fc30368b026b7d65fda7e'
